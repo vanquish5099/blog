d162472564e9483daba89ea5ba4195cc3cb49708
vanquish5099 <394315271@qq.com>
2023-03-04 14:21:27 +0800

    feat:项目初始化

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,48 @@
+import { defineConfig, DefaultTheme } from 'vitepress'
+
+export default defineConfig({
+    base:'/vanquish5099/',
+    title: `仙童`,
+    description: `童子的小屋`,
+    head: [
+        ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+        ['meta', { name: 'theme-color', content: '#646cff' }],
+    ],
+    vue: {
+        reactivityTransform: true,
+    },
+    themeConfig: {
+        logo: '/logo.svg',
+
+        nav: [
+            { text: '笔记', link: '/guide/', activeMatch: '/guide/' },
+            { text: '闲谈', link: '/config/', activeMatch: '/config/' },
+        ],
+
+        sidebar: {
+            '/guide/': [
+                {
+                    text: 'APIs',
+                    items: [
+                        {
+                            text: 'Plugin API',
+                            link: '/guide/api-plugin',
+                        },
+                        {
+                            text: 'HMR API',
+                            link: '/guide/api-hmr',
+                        },
+                        {
+                            text: 'JavaScript API',
+                            link: '/guide/api-javascript',
+                        },
+                        {
+                            text: 'Config Reference',
+                            link: '/config/',
+                        },
+                    ],
+                }
+            ]
+        }
+    }
+})
---
+++ docs/index.md
@@ -0,0 +1,44 @@
+---
+layout: home
+
+title: 潼子
+titleTemplate: vanquish
+
+hero:
+  name: 潼子
+  text: 学习札记
+  tagline: 内观自己，拜托焦虑；外观世界，借力前行。
+  image:
+    src: /logo.svg
+    alt: xt
+#   actions:
+#     - theme: brand
+#       text: Get Started
+#       link: /guide/
+#     - theme: alt
+#       text: Why Vite?
+#       link: /guide/why
+#     - theme: alt
+#       text: View on GitHub
+#       link: https://github.com/vitejs/vite
+
+# features:
+#   - icon: 💡
+#     title: Instant Server Start
+#     details: On demand file serving over native ESM, no bundling required!
+#   - icon: ⚡️
+#     title: Lightning Fast HMR
+#     details: Hot Module Replacement (HMR) that stays fast regardless of app size.
+#   - icon: 🛠️
+#     title: Rich Features
+#     details: Out-of-the-box support for TypeScript, JSX, CSS and more.
+#   - icon: 📦
+#     title: Optimized Build
+#     details: Pre-configured Rollup build with multi-page and library mode support.
+#   - icon: 🔩
+#     title: Universal Plugins
+#     details: Rollup-superset plugin interface shared between dev and build.
+#   - icon: 🔑
+#     title: Fully Typed APIs
+#     details: Flexible programmatic APIs with full TypeScript typing.
+---
